eb0b0090b62492d7e6762516a3088c9a0ba69206
Jwink3101 <Jwink3101@users.noreply.github.com>
2020-06-09 15:41:17 -0600

    Uncertainty in curves

--- run_and_export.py
+++ run_and_export.py
@@ -2,17 +2,34 @@
 
 # This could be easier done in bash but I prefer to just do it in python
 
-import glob,subprocess,os,shutil,shlex
+import glob,subprocess,os,shutil,sys
 
 os.chdir(os.path.dirname(__file__))
-for nb in glob.glob('*.ipynb'):
+
+files = sys.argv[1:]
+if not files:
+    files = glob.glob('*.ipynb')
+
+for nb in files:
     nb = os.path.splitext(nb)[0]
     
-    cmd = f'jupyter-nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute {nb}.ipynb --output {nb}.tmp.ipynb'
-    subprocess.check_call(shlex.split(cmd))
-    
+    cmd = ['jupyter-nbconvert',
+           '--ExecutePreprocessor.timeout=-1',
+           '--to','notebook',
+           '--execute',
+           f'{nb}.ipynb',
+           '--output',f'{nb}.tmp.ipynb']
+           
+    s = subprocess.call(cmd)
+    if s:
+        raise subprocess.CalledProcessError()
+        
     shutil.move(f'{nb}.tmp.ipynb',f'{nb}.ipynb')
     
-    
-    cmd = f'jupyter-nbconvert --to html --output {nb}.html {nb}.ipynb'
-    subprocess.check_call(shlex.split(cmd))
+    cmd = ['jupyter-nbconvert',
+           '--to','html',
+           '--output',f'{nb}.html',
+           f'{nb}.ipynb']
+    s = subprocess.call(cmd)
+    if s:
+        raise subprocess.CalledProcessError()
